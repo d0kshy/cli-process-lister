08b34501e806c965008c2b995230057fccb414e5
Daria <dariakurnosova.contact@gmail.com>
2026-01-09 17:15:46 +0100

    Updated process lister to show threads for each process

--- main.cpp
+++ main.cpp
@@ -9,29 +9,35 @@ int main() {
     std::cout << "------------------------------ CLI Process Lister ----------------------------\n";
 
     // 1. DEFINE VARIABLES
+    HANDLE hProcessSnap;
     HANDLE hThreadSnap;
+    PROCESSENTRY32 pe32;
     THREADENTRY32 te32;
 
+
+    pe32.dwSize = sizeof(PROCESSENTRY32);
     te32.dwSize = sizeof(THREADENTRY32);
 
 
     // 2. TAKE THE SNAPSHOT
+    hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
     hThreadSnap = CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, 0);
 
 
     // 3. CHECKING FOR ERRORS
-    if (hThreadSnap == INVALID_HANDLE_VALUE)
+    if (hProcessSnap == INVALID_HANDLE_VALUE || hThreadSnap == INVALID_HANDLE_VALUE)
     {
-        std::cout << "Error: Snap thread has an invalid handle value." << std::endl;
+        std::cout << "Error: Could not take snap." << std::endl;
 
         return -1;
     }
 
 
     // 4. GET THE FIRST PROCESS
-    if (!Thread32First(hThreadSnap, &te32))
+    if (!Process32First(hProcessSnap, &pe32))
     {
-        std::cout << "Error: The thread list was not found." << std::endl;
+        std::cout << "Error: The list was not found." << std::endl;
+        CloseHandle(hProcessSnap);
         CloseHandle(hThreadSnap);
 
         return -1;
@@ -39,22 +45,27 @@ int main() {
 
 
     // 5. LOOP THROUGH THE REST
+    do {
+
         std::cout << "------------------------------------------------------------------------------\n";
-        std::cout << std::left << std::setw(50) << "THREAD NAME" << std::setw(15) << "PID" << std::endl;
+        std::wcout << std::left << std::setw(21) << "PROCESS NAME" << pe32.szExeFile << std::setw(15) << "\n\tPID" << pe32.th32ProcessID << std::endl;
         std::cout << "------------------------------------------------------------------------------\n";
 
-        do {
-            if (te32.th32OwnerProcessID) {
-                std::wcout << std::left
-                    << std::setw(50) << te32.th32OwnerProcessID
-                    << std::setw(15) << te32.th32ThreadID
-                    << std::endl;
-            }
-        } while (Thread32Next(hThreadSnap, &te32));
-        
+        if (Thread32First(hThreadSnap, &te32))
+        {
+            do {
+                if (te32.th32OwnerProcessID == pe32.th32ProcessID) {
+                    std::cout << "   -> Thread ID: " << te32.th32ThreadID << "\n";
+                }
+
+            } while (Thread32Next(hThreadSnap, &te32));
+        }
+    } while (Process32Next(hProcessSnap, &pe32));
+
 
 
     // 6. CLEAN UP
+    CloseHandle(hProcessSnap);
     CloseHandle(hThreadSnap);
     std::cout << "------------------------------------------------------------------------------\n\n";
     std::cout << "SCANNING COMPLETE.\n\n\n";
